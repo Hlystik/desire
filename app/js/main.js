3b32608e4acbbe2be82c60cd657975b957302cbd
Anatoly <hlystike@rambler.ru>
2022-07-28 09:19:53 +0700

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -19,11 +19,55 @@ $(function () {
     });
 
     $('.contact-slider__wraper').slick({
-        items: 10,
+       /*  items: 10, */
         dots: true,
         arrows: false,
         slidesToShow: 10,
-        slidesToScroll: 10
+        slidesToScroll: 10,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 651,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
